23d65483a8fa05a35a4face76685c9e9c06ab436
ZephV <zephkwok@outlook.com>
2018-05-06 02:04:46 +0800

    add header

---
+++ vue-demo/去哪儿网/qunar/src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont icon-back">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-arrow">&#xe600;</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/veribles.styl'
+  .header
+    display:flex
+    height:.86rem
+    line-height:.86rem
+    background-color:$bgColor
+    color:#fff
+    .header-left
+      float:left
+      width:.64rem
+      .icon-back
+        text-align:center
+        font-size:.4rem
+    .header-input
+      flex 1
+      margin-top:.12rem
+      padding-left:.2rem
+      height:.62rem
+      line-height:.62rem
+      background-color:#fff
+      border-radius:.1rem
+      color:#ccc
+    .header-right
+      float:right
+      width:1.24rem
+      text-align:center
+      .icon-arrow
+        margin-left:-.04rem
+        font-size:.24rem
+</style>
